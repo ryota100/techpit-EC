a0118c4b39a61b886bc6c5199b35eba89772c17c
Your Name <you@example.com>
2020-06-09 08:09:13 +0000

    add buyfunction

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/item/{item}', 'ItemController@show');
 
 Route::post('/cartitem', 'CartItemController@store');
+
+Route::get('/cartitem', 'CartItemController@index');
+
+Route::delete('/cartitem/{cartItem}', 'CartItemController@destroy');
+
+Route::put('/cartitem/{cartItem}', 'CartItemController@update');
+
+Route::get('/buy', 'BuyController@index');
+Route::post('/buy', 'BuyController@store');
